85d0e0a59efdc11103b41e95a05c0d81382bac4a
Shirish Adhikari <169064642+Shirish0007@users.noreply.github.com>
2025-04-01 22:24:43 +0545

    .

---
+++ script.js
@@ -0,0 +1,14 @@
+function UpdateClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    const ampm = hours >=12 ?"PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0 )
+    const minutes = now.getMinutes().toString().padStart(2, 0 );
+    const seconds = now.getSeconds().toString().padStart(2, 0 );
+    const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+    document.getElementById("clock").textContent = timeString;
+
+}
+
+setInterval(UpdateClock, 1000);
